cf15ec896a99cb52633c89607fba545b620fb33b
nasttymain <82762126+nasttymain@users.noreply.github.com>
2026-04-26 19:10:18 +0900

    new default font

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 
 void proc_cin();
 
+uint8_t mode = 0;
+
 int main() {
     stdio_init_all();
     
@@ -18,21 +20,33 @@ int main() {
     clrgraph(1);
     while(1){
         //proc_cin();
-        clrgraph(1);
-        swimming_triangle::frame();
-        boxf(20, 20, 50, 30);
-        palcolor(0);
-        tvvt::pos(0, 0);
-        tvvt::puts("Hello, World!");
+        if(mode == 0){
+            clrgraph(1);
+            swimming_triangle::frame();
+            boxf(20, 20, 50, 30);
+            palcolor(0);
+            tvvt::pos(0, 0);
+            tvvt::puts("Hello, World!");
+        }
+        if(mode == 1){
+            
+        }
         wait_for_vsync();
-        /*
+        // BEGIN MODE_CHANGE
         if(frame % 300 == 0){
-            if((frame / 300) % 2 == 1){
-                setDisplayMode(SCREEN_GRAYSCALE);
-            }else{
+            mode = (mode + 1) % 2;
+            if(mode == 0){
                 setDisplayMode(SCREEN_PALETTE);
             }
-        }*/
+            if(mode == 1){
+                setDisplayMode(SCREEN_GRAYSCALE);
+                clrgraph(1);
+                palcolor(0);
+                tvvt::pos(0, 0);
+                tvvt::puts("LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR. EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST LABORUM.");
+            }
+        }
+        // END MODE_CHANGE
     }
 }
 
