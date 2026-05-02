83dd7b6eafbeaf5f7d2ac6ec5c64d6bb391fcdbe
nasttymain <82762126+nasttymain@users.noreply.github.com>
2026-05-02 22:30:39 +0900

    font demo

---
+++ font.hpp
@@ -0,0 +1,14 @@
+#include "rca.hpp"
+#include "rcavt.hpp"
+
+namespace fonts_show{
+    void draw(){
+        clrgraph(1);
+        for(int i = 0; i < 96; i += 1){
+            int x = _display_size_x / 2 - 64 + (i % 16) * 8;
+            int y = _display_size_y / 2 - 24 + (i / 16) * 8;
+            palcolor(COLOR_BLACK);
+            tvvt::put_char_graphic((char)(i + 32), x, y);
+        }
+    }
+}
